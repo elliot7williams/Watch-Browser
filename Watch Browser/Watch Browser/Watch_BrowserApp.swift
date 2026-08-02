3811a5675fe5767f3e74eec34fee310785769a14
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 21:56:07 -0400

    Add Watch Browser/Watch Browser/Watch_BrowserApp.swift

---
+++ Watch Browser/Watch Browser/Watch_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_BrowserApp.swift
+//  Watch Browser
+//
+//  Created by Elliot Williams on 2025-07-16.
+//
+
+import SwiftUI
+
+@main
+struct Watch_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
