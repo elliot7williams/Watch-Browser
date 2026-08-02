6c2d1104a1010ee69bc125b494c23dddc7ffee90
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 21:56:00 -0400

    Add Watch Browser/Watch BrowserTests/Watch_BrowserTests.swift

---
+++ Watch Browser/Watch BrowserTests/Watch_BrowserTests.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_BrowserTests.swift
+//  Watch BrowserTests
+//
+//  Created by Elliot Williams on 2025-07-16.
+//
+
+import Testing
+@testable import Watch_Browser
+
+struct Watch_BrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
